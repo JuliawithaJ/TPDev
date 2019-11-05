8eea2ba8eb2ce828f4f983346ae8b7c7bf2bc530
JuliaC <juls.cheroux@gmail.com>
2019-11-05 11:46:46 +0100

    commit

--- destinations.php
+++ destinations.php
@@ -1,3 +1,9 @@
 <?php
 include 'header.php';
 ?>
+<div class="another-one">
+    <h1>Destinations</h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam animi facilis perspiciatis ipsam harum enim, esse autem. Quidem temporibus perferendis delectus sit, obcaecati at! Quo enim consequatur ea officia modi.
+    </p>
+</div>
